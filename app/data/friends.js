d5ec7c1b9db42eed4eb3a970066559aafcc69c04
Jeremy Sexton <lordtaltosh@gmail.com>
2018-10-25 13:32:19 -0500

    Final push

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Jeremy",
+	"photo" : "https://scontent.fmkc1-1.fna.fbcdn.net/v/t1.0-9/26804497_4341972146146_7464530897417445083_n.jpg?_nc_cat=102&_nc_ht=scontent.fmkc1-1.fna&oh=cb369cc9766598431b50cea3142ce75b&oe=5C4A9A42",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Sam",
+	"photo" : "https://scontent.fmkc1-1.fna.fbcdn.net/v/t1.0-9/43339402_10215388084314028_7743367259166343168_n.jpg?_nc_cat=101&_nc_ht=scontent.fmkc1-1.fna&oh=684b6989679280ecb3a104fe70d0b0e0&oe=5C5629AB",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Lizzy",
+	"photo" : "https://scontent.fmkc1-1.fna.fbcdn.net/v/t1.0-9/43390541_10156086251358152_3064053312761364480_o.jpg?_nc_cat=100&_nc_ht=scontent.fmkc1-1.fna&oh=08ec8917eacb51eea379a9144f8490e8&oe=5C82944F",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
